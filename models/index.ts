b1702101eb513de1225a531bc64513813af68256
Martin Novoa <martinnovoar2@gmail.com>
2022-09-15 17:19:31 -0300

    Migrate bin and database setup to typescript

--- models/index.ts
+++ models/index.ts
@@ -1,18 +1,24 @@
 import fs from 'fs';
 import path from 'path';
-import { Sequelize } from 'sequelize';
+import sequelize, { Sequelize } from 'sequelize';
 
 const basename = path.basename(__filename);
 const config = require(`${__dirname}/../config/config`).development;
-const db = {};
+const db:any = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelizeInstance = new sequelize.Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config,
+);
 
 fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    // eslint-disable-next-line global-require
+    const model = require(path.join(__dirname, file))(sequelizeInstance, sequelize.DataTypes);
     db[model.name] = model;
   });
 
