ee07ef66181ca7d3c6d9b24ae37a9e67d781f34e
Elias Perez <perexlito@gmail.com>
2022-09-16 15:55:46 -0300

    model category added

---
+++ src/models/category.ts
@@ -0,0 +1,42 @@
+
+import {Model,InferAttributes,DataTypes,InferCreationAttributes, CreationOptional, Sequelize} from 'sequelize';
+const sequelize = new Sequelize(process.env.DB_HOST || '127.0.0.1');
+
+class Category extends Model <InferAttributes<Category>, InferCreationAttributes<Category>>  {
+  declare id : CreationOptional<number>;
+  declare name:string;
+  declare description:string | null;
+  declare image:string | null;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+Category.init({
+  id: {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      primaryKey : true,
+      autoIncrement : true
+  },
+  name:{
+    type : DataTypes.STRING,
+    allowNull : false
+  },
+  description: {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  image: {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE,
+}, {
+  sequelize,
+  paranoid: true,
+  timestamps: true,
+  tableName: 'categories',
+  modelName: 'Category',
+});
+
+export default Category
