12b2b5d2fe95ebbb90aba47ec9432dd18f143a67
Martin Novoa <martinnovoar2@gmail.com>
2022-09-18 13:24:38 -0300

    Change error handler to send error in json instead of rendering html

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import {
-  NextFunction, Request, Response,
+  NextFunction, Request, Response
 } from 'express';
 
 import indexRouter from './routes';
@@ -35,15 +35,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err.message });
 });
 
 export default app;
