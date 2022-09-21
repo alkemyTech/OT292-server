9d8b16dac47074f7557e79bc960024c64418716f
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-21 16:54:08 -0300

    add auth validations

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,17 +1,19 @@
 import { body, CustomValidator } from 'express-validator';
-import db from '../models';
 
 // const isValidEmail : CustomValidator = (value, {}) => {
 //   console.log('Console.log :::> Custom :', value);
 // };
 
-const existUser : CustomValidator = () => {
+// const existUser : CustomValidator = async (value : string, { req }) : Promise<Error | void> => { 
+// };
 
-};
+// const isCorrectPassword : CustomValidator = async ( value : string, {req}) => {
+// };
 
 export default [
   // body().custom(isValidEmail),
   body('email').not().isEmpty().withMessage('Cannot be empty'),
   body('email').isEmail().withMessage('Must be a valid email'),
   body('password').not().isEmpty().withMessage('Cannot be empty'),
+  // body('email').custom(existUser),
 ];
