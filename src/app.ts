55d12ca551d4c2823c20cc6b4b986c3bd8e2e9e6
RockStone <RockStoneStudios666@gmail.com>
2022-09-22 09:11:37 -0500

    import router

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import {
   NextFunction, Request, Response
 } from 'express';
 
+//Routes
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 import organizationRouter from './routes/organizations';
+import newsRouter from './routes/news';
 
 const createError = require('http-errors');
 const express = require('express');
@@ -29,7 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/organization',organizationRouter)
+app.use('/organization',organizationRouter);
+app.use('/news',newsRouter);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
