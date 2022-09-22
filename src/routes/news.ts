14cc42809b4ee27c2df85c51bc3688429d488a9d
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-22 20:58:07 -0300

    add route create news

---
+++ src/routes/news.ts
@@ -0,0 +1,11 @@
+import express, { Router } from 'express';
+import newsController from '../controllers/news';
+import newsValidator from '../validations/newsValidator';
+import verifyAdmin from '../middleware/verifyAdmin';
+import validateToken from '../middlewares/auth';
+
+const router : Router = express.Router();
+
+router.post('/', validateToken, verifyAdmin, newsValidator, newsController.create);
+
+export default router;
