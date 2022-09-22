7e7a7ee51b3a5ed0e8bb9c866766fd59a7321284
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-22 20:58:52 -0300

    add create new controller

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -1,10 +1,28 @@
-import { Request, Response } from 'express';
-import News from '../models/news';
+import { NextFunction, Request, Response } from 'express';
+import News, { News as NewsClass } from '../models/news';
 
 async function index(req : Request, res : Response) {
-  res.send(`${News.name} controller`);
+  res.json({ message: `${News.name} controller` });
 }
+
+/**
+ * Allows us to create News
+ * @param Request request
+ * @param Response respose
+ * @param NextFunction next
+ * @returns
+ */
+const create = async (req : Request, res: Response, next: NextFunction) => {
+  try {
+    const newsSaved : NewsClass = await NewsClass.create(req.body);
+
+    return res.status(201).json(newsSaved);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 export default {
   index,
+  create,
 };
