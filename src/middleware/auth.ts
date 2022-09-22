2fe96e058babbfcbd304ae95a6a701914df53e25
Alejandro J Gutierrez R <ajgr89@gmail.com>
2022-09-21 23:46:21 -0300

    add JWT_SECRET to .env.dist

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { verifyToken } from '../utils/jwt.handle';
 
 export default function validateToken(req: Request, res: Response, next: NextFunction) {
