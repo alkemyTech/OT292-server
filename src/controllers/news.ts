59e42ba3b73eae2f96ed159335e15e72c3f9b76b
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-23 01:57:24 -0300

    change status code

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -16,10 +16,10 @@ const deleteNews = async (req : Request, res: Response, next: NextFunction) => {
   try {
     const result : number = await db.News.destroy({ where: { id: req.params.id } });
 
-    if (result === 0) return res.status(400).json({ status: 400, message: 'Resource not finded' });
+    if (result === 0) return res.status(404).json({ status: 404, message: 'Resource not finded' });
     return res.sendStatus(204);
   } catch (error) {
-    return res.status(400).json(error);
+    return res.status(500).json(error);
   }
 };
 
