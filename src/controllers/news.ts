f229da9738df3bf5a628a4d4b08f4a0f12142c28
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-23 01:45:05 -0300

    add delete news controller

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -1,10 +1,29 @@
-import { Request, Response } from 'express';
-import News from '../models/news';
+import { NextFunction, Request, Response } from 'express';
+import db from '../models/index';
+import { News } from '../models/news';
 
 async function index(req : Request, res : Response) {
-  res.send(`${News.name} controller`);
+  res.json({ message: `${News.name} controller` });
 }
+
+/**
+ * Allows us delete a entry of News.
+ * @param req Request
+ * @param res Response
+ * @param next Next
+ */
+const deleteNews = async (req : Request, res: Response, next: NextFunction) => {
+  try {
+    const result : number = await db.News.destroy({ where: { id: req.params.id } });
+
+    if (result === 0) return res.status(400).json({ status: 400, message: 'Resource not finded' });
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 export default {
   index,
+  deleteNews,
 };
