75478f318c261e7a75bbe8b0c82abca1a541e3a0
fredyvaron <fvaron@itfip.edu.co>
2022-09-24 18:36:20 -0500

    Feature/ot292 50

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -7,16 +7,11 @@ const join = require('path').join;
 async function sendMail (to: string, data: any, email:string, subject:string) {
           // ruta template
           sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-          console.log(process.env.SENDGRID_API_KEY!, "apikey");
-console.log(process.env.SENDGRID_EMAIL_SENDER, "emailsender");
-          console.log("ingreso a service mail")
-console.log(subject)
           const str = read(join(__dirname, '../views/plantillaemail.ejs'), 'utf8');
 
           // body para el mail
         
           const body = ejs.compile(str)(data);
-          console.log(data)
           const msg = {
     to,
     from: email,
