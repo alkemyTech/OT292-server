06d4dc5a98a1503fb44b1b2a03dd99af574677f2
fredyvaron <fvaron@itfip.edu.co>
2022-09-24 18:44:36 -0500

    Feature/ot292 50

---
+++ src/services/mailService.ts
@@ -0,0 +1,33 @@
+import sgMail from '@sendgrid/mail';
+const ejs = require('ejs');
+const read = require('fs').readFileSync;
+const join = require('path').join;
+
+
+async function sendMail (to: string, data: any, email:string, subject:string) {
+          // ruta template
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+          console.log(process.env.SENDGRID_API_KEY!, "apikey");
+console.log(process.env.SENDGRID_EMAIL_SENDER, "emailsender");
+          console.log("ingreso a service mail")
+console.log(subject)
+          const str = read(join(__dirname, '../views/plantillaemail.ejs'), 'utf8');
+
+          // body para el mail
+        
+          const body = ejs.compile(str)(data);
+          const msg = {
+    to,
+    from: email,
+    subject,
+    html: body
+    
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error: unknown) {
+    console.error(error);
+  }
+}
+
+export default sendMail
