892e113a950059dd3a3a0d6775aa63afbd545953
RockStone <RockStoneStudios666@gmail.com>
2022-09-26 12:28:02 -0500

    create the router auth

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import usersRouter from './routes/users';
 import authRouter from './routes/auth';
 import organizationRouter from './routes/organizations';
 import sendMailRouter from  './routes/sendemail';
+import newRouter from './routes/news';
 
 const createError = require('http-errors');
 const express = require('express');
@@ -35,7 +36,8 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/organization',organizationRouter)
-app.use('/sender', sendMailRouter)
+app.use('/sender', sendMailRouter);
+app.use('/news',newRouter);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
