98425c23656669b5889b52bf1475c1f4ad80c9aa
Elias Perez <perexlito@gmail.com>
2022-09-26 17:49:35 -0300

    updateUser, route and validation added

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -18,6 +18,36 @@ async function deleteUser (req: Request, res: Response) {
     }
 }
 
+async function updateUser(req:Request, res: Response) {
+    const id = req.params.id;
+    const {firstName, lastName, photo} = req.body;
+    
+    const existingUser = await db.User.findByPk(id)
+    if(!existingUser) {
+        return res.status(404).json({ message: 'Username does not exist', status: 404 });
+    }
+   
+    const updateData= {
+        firstName: firstName || existingUser.firstName,
+        lastName: lastName || existingUser.lastName,
+        photo: photo || existingUser.photo
+    }
+
+    try {
+        await db.User.update(updateData,{
+            where: { id }
+        })
+        const userModified = await db.User.findByPk(id,{
+            attributes: {exclude: ["password"],},
+        })
+        res.status(200).json( {message: userModified?.toJSON(), status: 200} );
+        
+    } catch (error) {
+        return res.status(500).json({ message: 'Could not delete', status: 500 });
+    }
+}
+
 export {
-    deleteUser
+    deleteUser,
+    updateUser
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { deleteUser } from '../controllers/users';
+import { deleteUser, updateUser } from '../controllers/users';
 
 import verifyAdmin from '../middleware/verifyAdmin';
 import verifyToken from '../middleware/auth';
 import { getAll } from '../controllers/user';
+import validateToken from '../middleware/auth';
+import verifyOwnership from '../middleware/verifyOwnership';
+import userValidation from '../validations/user.validation';
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', verifyToken, verifyAdmin, getAll);
 
 router.delete('/:id', deleteUser)
+router.patch('/:id',validateToken,verifyOwnership, userValidation, updateUser)
+
 
 export default router;
