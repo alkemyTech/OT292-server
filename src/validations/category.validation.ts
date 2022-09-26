e1a73694494cb7ca18fb271bca3ae4a65e8f404c
Martin Novoa <martinnovoar2@gmail.com>
2022-09-26 17:31:57 -0300

    Add validator for category creation endpoint

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -1,4 +1,4 @@
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const deleteValidator = [
   param('id', 'Invalid id').exists().isInt(),
@@ -6,4 +6,9 @@ export const deleteValidator = [
 
 export const getDetailsValidator = [
   param('id', 'Invalid id').exists().isInt(),
+];
+
+export const createValidator = [
+  body('name', 'Invalid name').exists().isString(),
+  body('description', 'Invalid description').optional().isString(),
 ];
