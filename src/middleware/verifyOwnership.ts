7742e7299bd29657eac86a1d8673dc12aaa83c8b
Elias Perez <perexlito@gmail.com>
2022-09-26 17:47:59 -0300

    fix change userId

--- src/middleware/verifyOwnership.ts
+++ src/middleware/verifyOwnership.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import verifyAdmin from './verifyAdmin';
 
 export default async function verifyOwnership(req: Request, res: Response, next: NextFunction) {
-  const { id } = req.params;
-  const { userId } = req.body;
-  if (id === userId) {
+  const id = req.params.id;
+  const userId = req.userId;
+
+  if (Number(id) === Number(userId)) {
     return next();
   }
   return verifyAdmin(req, res, next);
