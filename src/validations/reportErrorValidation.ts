d82b735f9691eab15d35568405ca309247ee3ea4
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-27 20:50:43 -0300

    add error formater

--- src/validations/reportErrorValidation.ts
+++ src/validations/reportErrorValidation.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, ValidationError } from 'express-validator';
 import createError from 'http-errors';
 
+const errorValidationFormatter = ({
+  location, msg, param, value, nestedErrors,
+}: ValidationError) => `${location}[${param}]: ${msg}`;
+
 export default function reportError(req: Request, res: Response, next: NextFunction) {
-  const result = validationResult(req);
+  const result = validationResult(req).formatWith(errorValidationFormatter);
   if (!result.isEmpty()) {
     return next(createError(400, 'Input validation error', { expose: false, contents: result.array() }));
   }
