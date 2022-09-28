82a6572981e3ac6d86102b57a2c73db7bfdeffe9
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-28 20:56:52 -0300

    add createMember controller

--- src/controllers/members.ts
+++ src/controllers/members.ts
@@ -1,10 +1,32 @@
-import { Request, Response } from 'express';
-import Member from '../models/member';
+import { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import db from '../models/index';
+import { Member } from '../models/member';
 
-async function index(request: Request, response:Response) {
-  response.send(`${Member.name}`);
+export async function index(request: Request, response:Response) {
+  response.send(`${db.Member.name}`);
+}
+
+export async function createMember(req:Request, res: Response, next:NextFunction) {
+  try {
+    const memberSaved : Member = await db.Member.create({
+      name: req.body.name,
+      facebookUrl: req.body.facebookUrl || null,
+      instagramUrl: req.body.instagramUrl || null,
+      linkedinUrl: req.body.linkedinUrl || null,
+      image: req.body.image,
+    });
+
+    return res.status(201).json(memberSaved);
+  } catch (error : Error | any) {
+    if (error instanceof Error) {
+      return next(createHttpError(500, error.message, { expose: false }));
+    }
+    return res.status(500).json(error);
+  }
 }
 
 export default {
   index,
+  createMember,
 };
