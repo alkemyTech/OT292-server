2802989223d90a6fe682f8dd8841e5b54718d5c6
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-27 23:10:08 -0300

    add updateValidation and schemas

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -1,4 +1,50 @@
-import { param } from 'express-validator';
+import { param, checkSchema } from 'express-validator';
+import reportError from './reportErrorValidation';
+
+const schemaId = checkSchema({
+  id: {
+    in: ['params'],
+    exists: { errorMessage: 'Must provide an ID' },
+    isInt: { errorMessage: 'ID must be an integuer' },
+    toInt: true,
+  },
+});
+
+const schemaName = checkSchema({
+  name: {
+    in: ['body'],
+    notEmpty: {
+      errorMessage: 'Name cannot be empty',
+      bail: true,
+    },
+    isString: {
+      errorMessage: 'Name must be string',
+    },
+  },
+});
+
+const schemaDescription = checkSchema({
+  description: {
+    in: ['body'],
+    isString: {
+      errorMessage: 'Content must be string',
+    },
+    optional: { options: { nullable: true } },
+  },
+});
+
+const schemaImage = checkSchema({
+  image: {
+    in: ['body'],
+    notEmpty: {
+      errorMessage: 'Image cannot be empty',
+      bail: true,
+    },
+    isString: {
+      errorMessage: 'Image must be string',
+    },
+  },
+});
 
 export const deleteValidator = [
   param('id', 'Invalid id').exists().isInt(),
@@ -7,3 +53,17 @@ export const deleteValidator = [
 export const getDetailsValidator = [
   param('id', 'Invalid id').exists().isInt(),
 ];
+
+export const updateValidator = [
+  ...schemaId,
+  ...schemaName,
+  ...schemaDescription,
+  ...schemaImage,
+  reportError,
+];
+
+export default {
+  deleteValidator,
+  getDetailsValidator,
+  updateValidator,
+};
