18f31fdc9ac1ddfa74a6045e2c1fecbd95fe0cb5
Elias Perez <perexlito@gmail.com>
2022-09-27 22:17:44 -0300

    route register user added

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import authController from '../controllers/auth';
 import authValidation from '../validations/auth.validation';
+import registerUserValidator from '../validations/registerUser.validator';
 
 const router : express.Router = express.Router();
 
 router.post('/login', authValidation, authController.login);
+router.post('/register', registerUserValidator, authController.registerUser);
 
 export default router;
