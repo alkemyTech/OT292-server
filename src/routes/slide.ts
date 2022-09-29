9a74f495038ddc10e5f28621817f88c6d954720d
RockStone <RockStoneStudios666@gmail.com>
2022-09-29 16:13:28 -0500

    protected router slideDelete validateToken y VerifyAdmin and validateRead

--- src/routes/slide.ts
+++ src/routes/slide.ts
@@ -1,14 +1,17 @@
 import {Router} from 'express';
-import {slideDetail} from '../controllers/slides';
+import {slideDetail,slideDelete} from '../controllers/slides';
 import {validateRead} from '../validations/slideValidator';
 import VerifyAdmin from '../middleware/verifyAdmin';
 import ValidateToken from '../middleware/auth';
+import validateToken from '../middleware/auth';
 
 
 
 const router = Router();
 
+router.delete('/:id',validateToken,VerifyAdmin,validateRead,slideDelete);
 router.get('/:id',ValidateToken,VerifyAdmin,validateRead,slideDetail);
+
 
 
 
