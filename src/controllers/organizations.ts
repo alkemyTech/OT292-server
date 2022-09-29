6231a92ad1333882668a15e4115e119c1bb703de
Elias Perez <perexlito@gmail.com>
2022-09-29 09:05:19 -0300

    modified and fixed to return social media

--- src/controllers/organizations.ts
+++ src/controllers/organizations.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
-import db from '../models/index'
+import db from '../models/index';
 
-
-export const getOrganizationData = async (req: Request, res: Response) => {
-      try{
-          const organizations = await db.Organization.findAll({attributes : {exclude :["id","welcomeText","createdAt","updatedAt","deletedAt","aboutUsText","email"]}})
-           return res.status(200).json(organizations);
-        }catch(error){
-        return res.status(500).json(error);
-      }
-}
+export const getOrganizationData = async (_req: Request, res: Response) => {
+  try {
+    const organizations = await db.Organization.findOne({ attributes: { exclude: ['id', 'welcomeText', 'createdAt', 'updatedAt', 'deletedAt', 'aboutUsText', 'email'] } });
+    return res.status(200).json({ message: organizations, status: 200 });
+  } catch (error) {
+    return res.status(500).json({ message: error, status: 500 });
+  }
+};
 
