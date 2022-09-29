f3bc609602c2a6f6f8a0932e3952dd981cb0615b
RockStone <RockStoneStudios666@gmail.com>
2022-09-29 06:42:32 -0500

    create Router and protected route with validate token and admin

---
+++ src/routes/slide.ts
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+import {slideDetail} from '../controllers/slides';
+import {validateRead} from '../validations/slideValidator';
+import VerifyAdmin from '../middleware/verifyAdmin';
+import ValidateToken from '../middleware/auth';
+
+
+
+const router = Router();
+
+router.get('/:id',ValidateToken,VerifyAdmin,slideDetail);
+
+
+export default router;
