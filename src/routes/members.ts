b06035baafc19a891ca740448e678b06e5caf782
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-09-28 21:10:28 -0300

    fix auth authentication

--- src/routes/members.ts
+++ src/routes/members.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { validateCreation } from '../validations/member.validation';
 import { createMember } from '../controllers/members';
+import validateToken from '../middleware/auth';
 
 const router = express.Router();
 
-router.post('/', validateCreation, createMember);
+router.post('/', validateToken, validateCreation, createMember);
 
 export default router;
