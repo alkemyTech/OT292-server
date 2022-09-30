cb2f22b29c2169425780696f67df4ae2465002a2
fredyvaron <fvaron@itfip.edu.co>
2022-09-29 20:58:18 -0500

    feature/OT292-70

--- src/controllers/testimonials.ts
+++ src/controllers/testimonials.ts
@@ -2,21 +2,21 @@ import { Request, Response, NextFunction } from "express";
 import db from '../models/index';
 
 const create = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body, "Req total");
+
   const { name, content, image } = req.body;
   let newTestimonial;
-  if (name || content || image) {
     try {
       newTestimonial = await db.Testimonial.create({name, content, image});
       res.status(200).send(newTestimonial);
     } catch (error) {
       res.status(500).json(error);
     }
-  }else{
-    res.status(400).json({message: "Not Found name or content"})
-  }
 };
+const deletetestimonial = async (req:Request, res:Response) => {
+
+}
 
 export default {
   create,
+  deletetestimonial
 };
