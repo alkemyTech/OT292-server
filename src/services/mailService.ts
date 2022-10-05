a1a732e142c8ae85f03217da2371b1a730163b58
Martin Novoa <martinnovoar2@gmail.com>
2022-10-05 12:36:17 -0300

    Function to send contact email

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -1,23 +1,16 @@
 import sgMail from '@sendgrid/mail';
-const ejs = require('ejs');
-const read = require('fs').readFileSync;
-const join = require('path').join;
+import ejs from 'ejs';
+import { join } from 'path';
+import { readFileSync as read } from 'fs';
 
-
-async function sendMail (to: string, data: any, email:string, subject:string) {
-          // ruta template
-          sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
-          const str = read(join(__dirname, '../views/plantillaemail.ejs'), 'utf8');
-
-          // body para el mail
-        
-          const body = ejs.compile(str)(data);
-          const msg = {
+export async function sendContactEmail(to: string) {
+  const body = ejs.compile(read(join(__dirname, '../views/plantillaContacto.ejs'), 'utf8'))();
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+  const msg = {
     to,
-    from: email,
-    subject,
-    html: body
-    
+    from: process.env.SENDGRID_EMAIL_SENDER!,
+    subject: 'Contact',
+    html: body,
   };
   try {
     await sgMail.send(msg);
@@ -26,4 +19,26 @@ async function sendMail (to: string, data: any, email:string, subject:string) {
   }
 }
 
-export default sendMail
+async function sendMail(to: string, data: any, email: string, subject: string) {
+  // ruta template
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+  const str = read(join(__dirname, '../views/plantillaemail.ejs'), 'utf8');
+
+  // body para el mail
+
+  const body = ejs.compile(str)(data);
+  const msg = {
+    to,
+    from: email,
+    subject,
+    html: body,
+
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error: unknown) {
+    console.error(error);
+  }
+}
+
+export default sendMail;
