6dd66591047c37d0aae0835433d803f644d61442
RockStone <RockStoneStudios666@gmail.com>
2022-10-06 19:05:06 -0500

    create ValidateUpdate

--- src/validations/slideValidator.ts
+++ src/validations/slideValidator.ts
@@ -1,4 +1,4 @@
-import { body, checkSchema } from 'express-validator';
+import { body ,checkSchema } from 'express-validator';
 import reportError from './reportErrorValidation';
 
 const schemaId = checkSchema({
@@ -15,8 +15,72 @@ export const validateRead = [
   reportError,
 ];
 
+const schemaImageUrl = checkSchema({
+   imageUrl: {
+      in:['body'],
+      notEmpty :{
+        errorMessage : 'imageUrl cannot be empty',
+        bail : true
+      },
+      isString:{
+         errorMessage : 'imageUrl must be string'
+      },
+      optional: { options: { nullable: true } }
+   }
+})
+
+const schemaText = checkSchema({
+   text:{
+     in:['body'],
+     notEmpty: {
+      errorMessage: 'Name cannot be empty',
+      bail : true
+    },
+    isString: {
+      errorMessage: 'Name must be string',
+    },
+    optional: { options: { nullable: true } }
+   }
+});
+
+const schemaOrder = checkSchema({
+   order:{
+    in:['body'],
+    notEmpty:{
+      errorMessage : 'Order cannot be empty',
+      bail : true
+    },
+    isString :{
+      errorMessage : 'Order must be string'
+    },
+    optional: { options: { nullable: true } },
+   }
+});
+
+const schemaOrganizationId = checkSchema({
+   organizationId :{
+     in:['body'],
+     notEmpty:{
+       errorMessage : 'Organization cannot be empty'
+     },
+     isInt : {
+       errorMessage : 'Organization must be Integer'
+     },
+     optional: { options: { nullable: true } },
+   }
+})
+
 export const validateCreate = [
   body('text').exists().withMessage('Must provide a text'),
   body('order').exists().withMessage('Must provide an order'),
   body('organizationId').exists().withMessage('Must provide an organizationId'),
 ];
+
+export const validateUpdate = [
+   ...schemaId,
+   ...schemaImageUrl,
+   ...schemaText,
+   ...schemaOrder,
+   ...schemaOrganizationId,
+   reportError
+]
