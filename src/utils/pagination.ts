e07bf6c5381ababd791fec96a37758778e6c6e00
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-10-10 00:49:43 -0300

    refactor util pagination

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,18 +1,38 @@
 const port = process.env.PORT || 3000;
 
 const urlBase = process.env.APP_URL_PRODUCTION ? `${process.env.APP_URL_PRODUCTION}` : `http://localhost:${port}`;
-const pageString = '?page=';
 
-const calculatePage = async (total: number, page: number, url: string) => {
-  const currentPage: number = page;
-  const totalPages = Math.ceil(total / 10);
-  const previos = currentPage === 1 || currentPage > totalPages + 1 ? undefined : currentPage - 1;
+/**
+ * Create pagination url for different contexts
+ * @param relativeUrl Relative Url from the request
+ * @param numberPage Number page
+ * @param offset
+ * @param limit
+ * @returns URL string
+ */
+const buildUrl: Function = (
+  relativeUrl:string,
+  numberPage: number,
+  offset:number,
+  limit:number,
+) : string => (`${urlBase}${relativeUrl}?page=${numberPage}&offset=${offset}&limit=${limit}`);
+
+const calculatePage = (
+  total: number,
+  currentPage: number,
+  offset: number,
+  limit: number,
+  relativeUrl: string,
+) => {
+  const totalPages = Math.ceil(total / limit);
+  const previos = (currentPage === 1 || currentPage > totalPages + 1) ? undefined : currentPage - 1;
   const next = totalPages < currentPage + 1 ? undefined : currentPage + 1;
+
   return {
-    page: urlBase + url + pageString + currentPage,
-    next: next ? urlBase + url + pageString + next : undefined,
-    previus: previos ? urlBase + url + pageString + previos : undefined,
-    lastpage: totalPages === 0 ? undefined : urlBase + url + pageString + totalPages,
+    page: buildUrl(relativeUrl, currentPage, offset, limit),
+    next: next ? buildUrl(relativeUrl, next, offset, limit) : undefined,
+    previus: previos ? buildUrl(relativeUrl, previos, offset, limit) : undefined,
+    lastpage: totalPages === 0 ? undefined : buildUrl(relativeUrl, totalPages, offset, limit),
     count: totalPages,
   };
 };
