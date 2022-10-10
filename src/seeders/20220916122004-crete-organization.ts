9ce99e57b82207fc4805b3697060d19e2a44f05d
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-10-10 03:03:22 -0300

    add seeders

--- src/seeders/20220916122004-crete-organization.ts
+++ src/seeders/20220916122004-crete-organization.ts
@@ -1,22 +1,30 @@
 import { QueryInterface, Sequelize } from 'sequelize';
+import { faker } from '@faker-js/faker';
 
+const createRandomOrganization : Function = () : Object => ({
+  name: faker.company.name(),
+  image: faker.image.imageUrl(),
+  address: faker.address.direction(),
+  phone: faker.phone.number('##########'),
+  email: faker.internet.exampleEmail(),
+  welcome_text: faker.random.words(10),
+  facebook_url: faker.internet.url(),
+  linkedin_url: faker.internet.url(),
+  instagram_url: faker.internet.url(),
+  created_at: faker.date.past(1, new Date()),
+  updated_at: faker.datatype.boolean() ? faker.date.past(3, new Date()) : null,
+});
 
+const fillOrganizations : Function = (n:number) : Object[] => {
+  const activities : Object[] = [];
+  Array.from({ length: n }).forEach(() => activities.push(createRandomOrganization()));
+  return activities;
+};
+
+const organizations = fillOrganizations(100);
 module.exports = {
   up: async (queryInterface: QueryInterface, Sequelize: Sequelize) => {
-    await queryInterface.bulkInsert('Organizations', [{
-      name: 'myOrganization',
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      address: 'the doral',
-      phone: 5111223344556,
-      email: 'myorg@myorg.com',
-      welcome_text: 'welcome to my organization',
-      about_us_text: 'abouts us',
-      facebook_url: 'SomosMás',
-      linkedin_url: 'Somos_Más',
-      instagram_url: 'Somos_Más',
-      created_at: new Date,
-      updated_at: new Date
-    }], {});
+    await queryInterface.bulkInsert('Organizations', organizations, {});
   },
 
   down: async (queryInterface: QueryInterface, Sequelize: Sequelize) => {
--- src/seeders/20220929034731-create-slide.ts
+++ src/seeders/20220929034731-create-slide.ts
@@ -1,23 +1,33 @@
-import {QueryInterface,Sequelize} from 'sequelize'
+import { QueryInterface, Sequelize } from 'sequelize';
+import { faker } from '@faker-js/faker';
 
+const createRandomSlides : Function = () : Object => ({
+  image_Url: faker.image.imageUrl(),
+  text: faker.random.words(10),
+  order: faker.random.numeric(2),
+  organization_Id: 1,
+  created_at: faker.date.past(1, new Date()),
+  updated_at: faker.datatype.boolean() ? faker.date.past(3, new Date()) : null,
+});
+
+const fillSlides : Function = (n:number) : Object[] => {
+  const activities : Object[] = [];
+  Array.from({ length: n }).forEach(() => activities.push(createRandomSlides()));
+  return activities;
+};
+
+const slides = fillSlides(100);
 module.exports = {
-  async up (queryInterface:QueryInterface, Sequelize:Sequelize) {
-   await queryInterface.bulkInsert('Slides',[{
-        image_Url : "image.png",
-        text : "anything is what I will write here",
-        order : 1,
-        organization_Id : 1,
-        created_at : new Date,
-        updated_at : new Date
-   }], {})
+  async up(queryInterface:QueryInterface, Sequelize:Sequelize) {
+    await queryInterface.bulkInsert('Slides', slides, {});
   },
 
-  async down (queryInterface:QueryInterface, Sequelize : Sequelize) {
+  async down(queryInterface:QueryInterface, Sequelize : Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
