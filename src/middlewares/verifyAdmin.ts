5eec0976228fe9639b910c24713b7531ea589f9f
Federico Stagnaro <federico.stagnaro.ar@gmail.com>
2022-10-11 16:42:21 -0300

    add create http error

--- src/middlewares/verifyAdmin.ts
+++ src/middlewares/verifyAdmin.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import createHttpError from 'http-errors';
 import db from '../database/models';
 
 export default async function verifyAdmin(req: Request, res: Response, next: NextFunction) {
   const { userId } = req;
   const user = await db.User.findOne({ where: { id: userId } });
   if (!user) {
-    return res.status(401).json({ message: 'Unauthorized', status: 401 });
+    return next(createHttpError(401));
   }
   const role = await user.getRole();
   if (!role || role.name !== 'admin') {
-    return res.status(403).json({ message: 'Forbidden', status: 403 });
+    return next(createHttpError(403));
   }
   return next();
 }
