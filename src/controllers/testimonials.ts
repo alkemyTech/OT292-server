7ec50f85f7f3d91978dda8a929b5883bab87576f
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-10-14 12:50:30 -0300

    add http error in testimonials

--- src/controllers/testimonials.ts
+++ src/controllers/testimonials.ts
@@ -4,9 +4,9 @@ import db from '../models/index';
 import calculatePage from '../utils/pagination';
 
 const readAll = async (req: Request, res: Response, next: NextFunction) => {
-  const limit : number = parseInt(req.query.limit as string, 10) || 10;
-  const offset : number | undefined = parseInt(req.query.offset as string, 10) || 10;
-  const page : number = parseInt(req.query.page as string, 10) || 1;
+  const limit: number = parseInt(req.query.limit as string, 10) || 10;
+  const offset: number | undefined = parseInt(req.query.offset as string, 10) || 10;
+  const page: number = parseInt(req.query.page as string, 10) || 1;
 
   try {
     const testimonial = await db.Testimonial.findAndCountAll({
@@ -20,51 +20,48 @@ const readAll = async (req: Request, res: Response, next: NextFunction) => {
       message: { pagination: pages, testimonials: testimonial.rows },
       status: 200,
     });
-  } catch (error : Error | any) {
+  } catch (error: Error | any) {
     return next(createHttpError(500, error.message, { expose: false }));
   }
 };
 
-const create = async (req: Request, res: Response) => {
+const create = async (req: Request, res: Response, next: NextFunction) => {
   const { name, content, image } = req.body;
   let newTestimonial;
   try {
     newTestimonial = await db.Testimonial.create({ name, content, image });
-    res.status(200).send(newTestimonial);
-  } catch (error) {
-    res.status(500).json(error);
+    return res.status(200).send(newTestimonial);
+  } catch (error: Error | any) {
+    return next(createHttpError(500, error.message, { expose: false }));
   }
 };
 
-const remove = async (req:Request, res:Response) => {
+const remove = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
     const destroy = await db.Testimonial.destroy({ where: { id } });
-    if (!destroy) return res.status(404).json({ status: 404, message: 'Testimonial not found' });
+    if (!destroy) return next(createHttpError(404, 'Testimonial not found'));
     return res.status(200).json({ status: 200, message: 'Testimonial deleted' });
-  } catch (error) {
-    return res.status(500).json(error);
+  } catch (error : Error | any) {
+    return next(createHttpError(500, error.message, { expose: false }));
   }
 };
-const update = async (req:Request, res:Response) => {
+const update = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   const { name, content, image } = req.body;
   let updated;
 
   try {
     updated = await db.Testimonial.findByPk(id);
-    if (!updated) return res.status(404).json({ message: 'No found testimonial' });
+    if (!updated) return next(createHttpError(404, 'Testimonial not found'));
     if (name) updated.name = name;
     if (content) updated.content = content;
     if (image) updated.image = image;
-    try {
-      updated.save();
-    } catch (error) {
-      return res.status(500).json(error);
-    }
+    updated.save();
+
     return res.status(200).json({ status: 200, message: updated });
-  } catch (error) {
-    return res.status(400).json(error);
+  } catch (error : Error | any) {
+    return next(createHttpError(500, error.message, { expose: false }));
   }
 };
 
--- src/routes/testimonials.ts
+++ src/routes/testimonials.ts
@@ -4,11 +4,11 @@ import verifyAdmin from '../middlewares/verifyAdmin';
 import validator from '../validations/testimonialValidator';
 import validateToken from '../middlewares/authenticateToken';
 
-const router:Router = express.Router();
+const router: Router = express.Router();
 
 router.post('/', validateToken, verifyAdmin, validator.validateCreation, controller.create);
-router.get('/', validateToken, controller.readAll);
-router.put('/:id', validateToken, verifyAdmin, controller.update);
+router.get('/', validateToken, validator.validateReadAll, controller.readAll);
+router.put('/:id', validateToken, verifyAdmin, validator.validateUpdate, controller.update);
 router.delete('/:id', validateToken, verifyAdmin, validator.validateDelete, controller.remove);
 
 export default router;
