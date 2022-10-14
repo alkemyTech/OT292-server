4afed969e5b235a748972059451921889b53ba58
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-10-14 12:08:10 -0300

    add http errors in members

--- src/controllers/members.ts
+++ src/controllers/members.ts
@@ -4,13 +4,13 @@ import db from '../models/index';
 import { Member } from '../models/member';
 import calculatePage from '../utils/pagination';
 
-export async function index(request: Request, response:Response) {
+export async function index(request: Request, response: Response) {
   response.send(`${db.Member.name}`);
 }
 
-export async function create(req:Request, res: Response, next:NextFunction) {
+export async function create(req: Request, res: Response, next: NextFunction) {
   try {
-    const memberSaved : Member = await db.Member.create({
+    const memberSaved: Member = await db.Member.create({
       name: req.body.name,
       facebookUrl: req.body.facebookUrl || null,
       instagramUrl: req.body.instagramUrl || null,
@@ -19,17 +19,14 @@ export async function create(req:Request, res: Response, next:NextFunction) {
     });
 
     return res.status(201).json(memberSaved);
-  } catch (error : Error | any) {
-    if (error instanceof Error) {
-      return next(createHttpError(500, error.message, { expose: false }));
-    }
-    return res.status(500).json(error);
+  } catch (error: Error | any) {
+    return next(createHttpError(500, error.message, { expose: false }));
   }
 }
 
 export const update = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const member : Member | null = await db.Member.findByPk(req.params.id);
+    const member: Member | null = await db.Member.findByPk(req.params.id);
     if (!member) { return next(createHttpError(404)); }
 
     member.name = req.body.name;
@@ -41,16 +38,16 @@ export const update = async (req: Request, res: Response, next: NextFunction) =>
     await member.save();
 
     return res.status(200).json({ message: member, status: 200 });
-  } catch (error:Error | any) {
+  } catch (error: Error | any) {
     return next(createHttpError(500, error.message, { expose: false }));
   }
 };
 
-export async function readAll(req:Request, res: Response, next:NextFunction) {
+export async function readAll(req: Request, res: Response, next: NextFunction) {
   try {
-    const limit : number = parseInt(req.query.limit as string, 10) || 10;
-    const offset : number | undefined = parseInt(req.query.offset as string, 10) || 10;
-    const page : number = parseInt(req.query.page as string, 10) || 1;
+    const limit: number = parseInt(req.query.limit as string, 10) || 10;
+    const offset: number | undefined = parseInt(req.query.offset as string, 10) || 10;
+    const page: number = parseInt(req.query.page as string, 10) || 1;
 
     const result = await db.Member.findAndCountAll({
       attributes: ['name'],
@@ -62,19 +59,19 @@ export async function readAll(req:Request, res: Response, next:NextFunction) {
     return res.status(200).json(
       { message: { pagination: pages, members: result.rows }, status: 200 },
     );
-  } catch (error : Error | any) {
+  } catch (error: Error | any) {
     return next(createHttpError(500, error.message, { expose: false }));
   }
 }
-export async function remove(req:Request, res: Response) {
+export async function remove(req: Request, res: Response, next: NextFunction) {
   const { id } = req.params;
   let membersdelete;
   try {
     membersdelete = await db.Member.destroy({ where: { id } });
     if (!membersdelete) return res.status(404).json({ status: 'Menber Not Found' });
-    return res.status(200).json({ status: '200', message: 'Menbers deleted successfully' });
-  } catch (error) {
-    return res.status(500).json(error);
+    return res.status(200).json({ status: '200', message: 'Members deleted successfully' });
+  } catch (error: Error | any) {
+    return next(createHttpError(500, error.message));
   }
 }
 
