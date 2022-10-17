9c8958b6246e555eebc42ae758ebaf1fd357e3f9
Elias Perez <perexlito@gmail.com>
2022-10-17 09:09:40 -0300

    delete routes /este /prueba

--- src/routes/organizations.ts
+++ src/routes/organizations.ts
@@ -1,23 +1,13 @@
-import { Router, Request, Response } from 'express';
-import imgUploadFile from '../services/upload';
+import { Router } from 'express';
 import controller from '../controllers/organizations';
 import validateToken from '../middlewares/authenticateToken';
 import verifyAdmin from '../middlewares/verifyAdmin';
 import validator from '../validations/organization.validation';
 
 const router = Router();
-const multer = require('multer');
 
-const upload = multer({ dest: 'uploads/' });
-
-async function uploadFile(req: Request, res: Response) {
-  const uploadImage = await imgUploadFile(req.file);
-  res.json(uploadImage);
-}
 router.get('/index', controller.index);
 router.get('/public', controller.readDetails);
-router.get('/este', controller.readDetails);
-router.post('/prueba', upload.single('image'), uploadFile);
 router.post('/public', validateToken, verifyAdmin, validator.update, controller.update);
 
 export default router;
