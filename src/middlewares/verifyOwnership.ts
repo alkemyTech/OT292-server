626f6aebf92be3f827af8bddc20b9e1ca239aeb2
fredyvaron <fvaron@itfip.edu.co>
2022-10-21 21:47:14 -0500

    feature/OT292-84

--- src/middlewares/verifyOwnership.ts
+++ src/middlewares/verifyOwnership.ts
@@ -1,12 +1,43 @@
 import { NextFunction, Request, Response } from 'express';
-import verifyAdmin from './verifyAdmin';
+/* import createHttpError from 'http-errors'; */
+import { Role } from '../models/role';
+import db from '../models/index';
 
 export default async function verifyOwnership(req: Request, res: Response, next: NextFunction) {
-  const { id } = req.params;
+  const idcoment = Number(req.params.id);
   const { userId } = req;
+  let usersearch;
+  let coment;
 
-  if (Number(id) === userId) {
-    return next();
+  try {
+    usersearch = await db.User.findOne({
+      where: { id: userId },
+      attributes: ['id', 'firstName', 'roleId'],
+      include: {
+        model: Role,
+        attributes: ['name'],
+      },
+    });
+    if (!usersearch) {
+      return res.status(401).json({ status: '401', message: 'Not found user' });
+    }
+  } catch (error: Error | any) {
+    return res.status(500).json({ status: '500', message: error });
   }
-  return verifyAdmin(req, res, next);
+
+  try {
+    coment = await db.Comment.findOne({ where: { id: idcoment }, attributes: ['userId'] });
+    if (!coment) {
+      return res.status(404).json({ status: '404', message: 'Not found coment' });
+    }
+    if (usersearch.roleId === 1) {
+      return next();
+    }
+    if (usersearch.id === coment.userId) {
+      return next();
+    }
+    return res.status(401).json({ status: '401', message: 'No tiene permisos' });
+  } catch (error: Error | any) {
+    return res.status(500).json({ status: '500', message: error });
+  }
 }
