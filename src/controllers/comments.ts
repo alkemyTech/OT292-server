a36c388add2762aa73e9722c2ea853c9e2876ae0
Juan <juanvera2001@hotmail.com>
2022-10-22 20:56:25 -0300

    update comment:authorize user

--- src/controllers/comments.ts
+++ src/controllers/comments.ts
@@ -35,7 +35,55 @@ const createComment = async (req: Request, res:Response, next: NextFunction) =>{
       }
   };
 
+async function updateComment(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.body;
+    const { body } = req.body;
+  
+    const existingComment = await db.Comment.findByPk(id);
+    if (!existingComment) {
+      return next(createHttpError(404, 'Comment does not exist'));
+    }
+
+    if(req.userId !==existingComment.userId 
+      && !(await currentUserIsAdmin(req))){
+        console.log("req.userId:"+req.userId);
+        console.log("currentUserisAdmin:"+await currentUserIsAdmin(req));
+      return next (createHttpError(403,'You are not authorized to realize this action'));
+    }
+    const updateData = {
+      body: body || existingComment.body,
+    };
+  
+    try {
+      await db.Comment.update(updateData, {
+        where: { id },
+      });
+      const commentModified = await db.Comment.findByPk(id);
+      return res.status(200).json({ message: commentModified?.toJSON(), status: 200 });
+    } catch (error: Error | any) {
+      return next(createHttpError(500, error.message, { expose: false }));
+    }
+}
+
+async function currentUserIsAdmin(req:Request):Promise<boolean> {
+  const { userId } = req;
+  const user = await db.User.findOne({ where: { id: userId } });
+  if (!user) {
+    console.log("Usuario no existe");
+    return false;
+  }
+  const role = await user.getRole();
+  if (!role || role.name !== 'admin') {
+    console.log("El usuario no es admin");
+    return false;
+  }
+  console.log("current user is admin");
+  return true;  
+}
+
+
 export default {
   listComments,
-  createComment
+  createComment,
+  updateComment
 };
--- src/validations/commentValidator.ts
+++ src/validations/commentValidator.ts
@@ -1,10 +1,18 @@
 import {body} from "express-validator"
+import reportError from "./reportErrorValidation"
 
 export const validateCreateComment= [
     body('body').exists().withMessage('Must provide a body'),
     body('newsId').exists().withMessage('Must provide a newsId')
 ]
 
+export const validateUpdateComment = [
+    body('id').exists().withMessage('Must provide a commentId'),
+    body('body').exists().withMessage('Must provide a body'),
+    reportError
+]
+
 export default{
-    validateCreateComment
+    validateCreateComment,
+    validateUpdateComment
 }
