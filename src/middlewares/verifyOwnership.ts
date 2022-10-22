7b56ebbde2916aafda850bb329c96022414f1b73
fredyvaron <fvaron@itfip.edu.co>
2022-10-22 10:49:06 -0500

    change response errors in ownership

--- src/middlewares/verifyOwnership.ts
+++ src/middlewares/verifyOwnership.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-/* import createHttpError from 'http-errors'; */
+import createHttpError from 'http-errors';
 import { Role } from '../models/role';
 import db from '../models/index';
 
@@ -19,16 +19,16 @@ export default async function verifyOwnership(req: Request, res: Response, next:
       },
     });
     if (!usersearch) {
-      return res.status(401).json({ status: '401', message: 'Not found user' });
+      return next(createHttpError(401));
     }
   } catch (error: Error | any) {
-    return res.status(500).json({ status: '500', message: error });
+    return next(createHttpError(500));
   }
 
   try {
     coment = await db.Comment.findOne({ where: { id: idcoment }, attributes: ['userId'] });
     if (!coment) {
-      return res.status(404).json({ status: '404', message: 'Not found coment' });
+      return next(createHttpError(404));
     }
     if (usersearch.roleId === 1) {
       return next();
@@ -36,8 +36,8 @@ export default async function verifyOwnership(req: Request, res: Response, next:
     if (usersearch.id === coment.userId) {
       return next();
     }
-    return res.status(401).json({ status: '401', message: 'No tiene permisos' });
+    return next(createHttpError(401));
   } catch (error: Error | any) {
-    return res.status(500).json({ status: '500', message: error });
+    return next(createHttpError(500));
   }
 }
