101a7d1e695cd626b562e97d176c1eb0ae567929
Elias Perez <perexlito@gmail.com>
2022-10-25 23:31:06 -0300

    add upload image

--- src/routes/news.ts
+++ src/routes/news.ts
@@ -3,13 +3,14 @@ import controller from '../controllers/news';
 import validator from '../validations/newsValidator';
 import verifyAdmin from '../middlewares/verifyAdmin';
 import validateToken from '../middlewares/authenticateToken';
+import uploadMiddleware from '../services/multer';
 
 const router: Router = express.Router();
 
-router.post('/', validateToken, verifyAdmin, validator.validateCreation, controller.create);
+router.post('/', validateToken, verifyAdmin, uploadMiddleware.single('image'), validator.validateCreation, controller.create);
 router.get('/', validateToken, verifyAdmin, validator.validateReadAll, controller.readAll);
 router.get('/:id', validateToken, verifyAdmin, validator.validateRead, controller.readDetails);
-router.put('/:id', validateToken, verifyAdmin, validator.validateUpdate, controller.update);
+router.put('/:id', validateToken, verifyAdmin, uploadMiddleware.single('image'), validator.validateUpdate, controller.update);
 router.delete('/:id', validateToken, verifyAdmin, validator.validateDelete, controller.remove);
 router.get('/:id/comments', validateToken, validator.validateRead, controller.getComments);
 
